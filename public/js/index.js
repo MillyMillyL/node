aa266424bd1a33894d18d15bb2a47284fed78268
Milly <liuhongmein@gmail.com>
2024-02-24 19:56:19 -0500

    babel

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+import '@babel/polyfill';
+import { login } from './login';
+import { displayMap } from './mapbox';
+
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+document.querySelector('.form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  login(email, password);
+});
