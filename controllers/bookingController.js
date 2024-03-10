609752e591dee4603a78c7eff32418c21a5dda70
Milly <liuhongmein@gmail.com>
2024-03-10 19:21:58 -0400

    await createBookingCheckout

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,15 +54,15 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].total / 100;
-  console.log(tour, user, price);
+
   await Booking.create({ tour, user, price });
 };
 
-exports.webhookCheckout = (req, res, next) => {
-  const signature = req.headers['stripe-signature'];
+exports.webhookCheckout = async (req, res, next) => {
   let event;
 
   try {
+    const signature = req.headers['stripe-signature'];
     event = stripe.webhooks.constructEvent(
       req.body,
       signature,
@@ -73,7 +73,7 @@ exports.webhookCheckout = (req, res, next) => {
   }
 
   if (event.type === 'checkout.session.completed')
-    createBookingCheckout(event.data.object);
+    await createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
 };
